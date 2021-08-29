cf13732eda6eb5081b738033940cc980d8b86dbe
hariharan s <shariharan6666@gmail.com>
2021-08-29 23:27:33 +0530

    search update

--- ui/assets/js/mybooking.js
+++ ui/assets/js/mybooking.js
@@ -61,4 +61,20 @@ function cancelBooking(id, rev, movieId, movieName, theatreName, ticket, date, t
     })
 
 }
+function searchName() {
+    let searchName = document.getElementById("searchBox").value;
+    let myTable = document.getElementById("myTable");
+    let tableRow = myTable.getElementsByTagName("tr");
+    for (var i = 0; i < tableRow.length; i++) {
+        let tableDatas = tableRow[i].getElementsByTagName("td")[1];
+        if (tableDatas) {
+            let textValue = tableDatas.textContent.toLowerCase() || tableDatas.innerText.toLowerCase();
+            if (textValue.indexOf(searchName) > -1) {
+                tableRow[i].style.display = "";
+            } else {
+                tableRow[i].style.display = "none";
+            }
+        }
+    }
+}
 
